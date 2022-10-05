7afbef8bfd3ce6c47312143b6007ff1f4fb4da3c
Austin Donovan <iskarr@Austins-MacBook-Pro.local>
2022-10-05 10:02:45 -0600

    added render API to edit, create and delete methods

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,7 +8,8 @@ export default function Navbar() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <NavLink className="navbar-brand" to="/contacts">
           <img
-            style={{ width: 25 + "%" }}
+            style={{ width: 25 + "%", marginLeft: "10px" }}
+            alt="MongoDB icon"
             src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png"
           ></img>
         </NavLink>
